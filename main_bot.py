b7f92567f507785d3c457d7d5c7be8b2987bbbfa
monoluntik <narbkv07@gmail.com>
2022-01-02 17:00:37 +0600

    black

--- main_bot.py
+++ main_bot.py
@@ -4,8 +4,10 @@ from aiogram import Bot, Dispatcher, types, executor
 import asyncio
 
 
-from config import token # Токен бота, я сохранил в файле, файл находится в .gitignore
-from pars_binance import GetPriceUSDT
+from config import (
+    token,
+)  # Токен бота, я сохранил в файле, файл находится в .gitignore
+from pars_binance import PriceUSDT
 
 
 bot = Bot(token=token)
@@ -13,59 +15,78 @@ bot = Bot(token=token)
 dp = Dispatcher(bot=bot)
 logging.basicConfig(level=logging.INFO)
 
-liiist = []
+users_list = []
 
 DELAY = 30
 
-buttons = ["LTC", "BTC", "ETH", "ADA", "VET", "SOL", "LUNA", "BNB",]
+buttons = [
+    "LTC",
+    "BTC",
+    "ETH",
+    "ADA",
+    "VET",
+    "SOL",
+    "LUNA",
+    "BNB",
+]
+
+
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    s = GetPriceUSDT(id = message.chat.id)
-    for i in liiist:
-        if i.id == message.chat.id:
-            liiist.pop(liiist.index(i))
-    liiist.append(s)
+    s = PriceUSDT(id=message.chat.id)
+    for user in users_list:
+        if user.id == message.chat.id:
+            users_list.pop(users_list.index(user))
+    users_list.append(s)
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*buttons)
     await message.answer("Выберите крипту", reply_markup=keyboard)
 
-@dp.message_handler(commands=['stop'])
+
+@dp.message_handler(commands=["stop"])
 async def send_by(message: types.Message):
-    for i in liiist:
-        if i.id == message.chat.id:
-            i.deactivate()
-            await message.reply(f"Stop {i.coin}")
+    for user in users_list:
+        if user.id == message.chat.id:
+            user.deactivate()
+            await message.reply(f"Stop {user.coin}")
+
 
 @dp.message_handler()
 async def with_puree(message: types.Message):
-    if message.text in buttons:
-        for i in liiist:
-            if i.id == message.chat.id:
-                i.set_coin(message.text)
-                if i.list_ == []:
-                    i.activate()
-                    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                    butt = ["/stop", "/start"]
-                    keyboard.add(*butt)
-                    await message.reply(f"Start {i.coin}", reply_markup=keyboard)
-    else:
-        await message.reply("Я тебя не понимаю. Напиши /start")
-    
 
-async def send_price():
-    for i in liiist:
-        if i.list_ != []:
-            new_message = i.get_message()
-            id = i.id
+    if not message.text in buttons:
+        await message.reply("Я тебя не понимаю. Напиши /start")
+        return
+
+    for user in users_list:
+
+        if user.id != message.chat.id:
+            return
+
+        user.set_coin(message.text)
+
+        if not user.status:
+            user.activate()
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            butt = ["/stop", "/start"]
+            keyboard.add(*butt)
+            await message.reply(f"Start {user.coin}", reply_markup=keyboard)
+
+
+async def send_messsage():
+    for user in users_list:
+        if not user.status:
+            new_message = user.get_message()
             if new_message != "Измеменений нет!":
-                await dp.bot.send_message(id, new_message)
+                await dp.bot.send_message(user.id, new_message)
 
 
 def repeat(coro, loop):
     asyncio.ensure_future(coro(), loop=loop)
     loop.call_later(DELAY, repeat, coro, loop)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.call_later(DELAY, repeat, send_price, loop)
+    loop.call_later(DELAY, repeat, send_messsage, loop)
     executor.start_polling(dp, loop=loop)
--- pars_binance.py
+++ pars_binance.py
@@ -4,20 +4,19 @@ import json
 from datetime import datetime
 
 
-class GetPriceUSDT():
-
+class PriceUSDT:
     def __init__(self, id):
-        self.list_ = [] 
+        self.status = []
         self.coin = None
-        self.id = id 
+        self.id = id
 
     def set_coin(self, coin):
         self.coin = coin
         self.old_price = self.get_bnb_price()
-        self.old_time = datetime.now() 
+        self.old_time = datetime.now()
 
     def activate(self):
-        self.list_.append(self)
+        self.status.append(self)
 
     def deactivate(self):
         self.list_.clear()
@@ -26,26 +25,24 @@ class GetPriceUSDT():
         if self.coin != None:
             symbol = self.coin
             URL = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
-            responce = requests.get(URL, headers={'User-agent':'your bot 0.1'})
+            responce = requests.get(
+                URL, headers={"User-agent": "your bot 0.1"}
+            )
             json_obj = responce.text
             python_obj = json.loads(json_obj)
             a = python_obj.get("price")
-            k = float(a) 
+            k = float(a)
             return round(k, 2)
 
     def get_message(self):
         new_price = self.get_bnb_price()
         new_time = datetime.now()
-        if round(new_price-self.old_price, 2) != 0:
+        if round(new_price - self.old_price, 2) != 0:
             new_message = f"""{self.coin}\nOld price: {self.old_price}\n{self.old_time}
             \nNew price: {new_price}\n{new_time}
-            \n{"The price fell by" if new_price<self.old_price else "The price went up by"}: {round(abs(new_price-self.old_price), 2)}"""
+            \n{"The price fell by" if new_price < self.old_price else "The price went up by"}: 
+            {round(abs(new_price-self.old_price), 2)}"""
             self.old_price = new_price
             self.old_time = new_time
             return new_message
         return f"Измеменений нет!"
-
-
-
-if __name__ == '__main__':
-    GetPriceUSDT()
